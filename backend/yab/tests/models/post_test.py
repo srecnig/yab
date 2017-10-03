5341032fb4945f418c13a88e26c885dde28c1634
Martin Sereinig <ms@usersnap.com>
2017-10-03 07:13:39 +0200

    using factory boy also in post test

--- backend/yab/tests/models/post_test.py
+++ backend/yab/tests/models/post_test.py
@@ -2,8 +2,9 @@ import pytest
 
 from sqlalchemy.exc import IntegrityError
 
-from yab.tests import ModelTest
 from yab.models import db, Post
+from yab.tests import ModelTest
+from yab.tests.factories import PostFactory
 
 
 class PostTest(ModelTest):
@@ -12,18 +13,18 @@ class PostTest(ModelTest):
 
     def test_title_must_exist(self):
         with pytest.raises(IntegrityError):
-            post = Post(title=None, body="Some body")
+            post = PostFactory.build(title=None)
             db.session.add(post)
             db.session.commit()
 
     def test_body_must_exist(self):
         with pytest.raises(IntegrityError):
-            post = Post(title="Some title", body=None)
+            post = PostFactory.build(body=None)
             db.session.add(post)
             db.session.commit()
 
     def test_published_at_default(self):
-        post = Post(title="Some title", body="Some body", published_at=None)
+        post = PostFactory.build(published_at=None)
         db.session.add(post)
         db.session.commit()
         assert post.published_at is not None
@@ -31,7 +32,7 @@ class PostTest(ModelTest):
     # other
 
     def test_public_dict_must_not_contain_meta(self):
-        post = Post(title="Some title", body="Some body")
+        post = PostFactory.build()
         db.session.add(post)
         db.session.commit()
 
