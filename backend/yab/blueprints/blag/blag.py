f7d9e0dc15255e6897f64053d1e313673cdaf5aa
Martin Sereinig <ms@usersnap.com>
2017-10-03 07:13:55 +0200

    blag should return json

--- backend/yab/blueprints/blag/blag.py
+++ backend/yab/blueprints/blag/blag.py
@@ -1,20 +1,20 @@
+from flask import jsonify
 from flask import abort, Blueprint
 
 from yab.models import Post
 
 blag = Blueprint('blag', __name__, template_folder='templates', static_folder='static', static_url_path='/static/blag')
 
-@blag.route('/')
 @blag.route('/posts')
 def post_index():
     posts = Post.query.all()
-    return 'welcome to my blag: %r' % [p.title for p in posts]
+    return jsonify([post.to_public_dict() for post in posts])
 
 
 @blag.route('/posts/<int:post_id>')
 def post_show(post_id):
     post = Post.query.filter_by(id=post_id).first()
     if post:
-        return 'i am the post: %r, here\'s my content: %r' % (post.title, post.body)
+        return jsonify(post.to_public_dict())
     else:
         abort(404)
--- backend/yab/tests/blueprints/blag/blag_test.py
+++ backend/yab/tests/blueprints/blag/blag_test.py
@@ -6,25 +6,22 @@ from yab.tests.factories import PostFactory
 
 
 class PostTest(BlueprintTest):
-    def test_get_root_index(self):
-        self._create_posts()
-        response = self.client.get("/")
-        assert response.status_code == 200
-
     def test_get_index(self):
         self._create_posts()
         response = self.client.get("/posts")
         assert response.status_code == 200
+        assert response.content_type == "application/json"
 
     def test_get_show(self):
         self._create_posts()
         response = self.client.get("/posts/%s" % self.post_a.id)
         assert response.status_code == 200
+        assert response.content_type == "application/json"
 
     def test_get_show_returns_404(self):
         response = self.client.get("/posts/1")
         assert response.status_code == 404
-
+        assert response.content_type == "application/json"
 
     def _create_posts(self):
         self.post_a = PostFactory.build()
